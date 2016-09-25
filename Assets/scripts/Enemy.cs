cae55b736b6dbe509ec4bcb5feb705113a77cb2f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-25 14:17:14 +0100

    Add Enemy class

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -3,5 +3,24 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
-	
+	private Rigidbody2D rb;
+	private float patrolTime = 1;
+	private float moveFroce = 200;
+	private float patrolTimer;
+	private Vector2 direction;
+
+	private void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		direction = Vector2.right * (Random.Range (0f, 1f) > 0.5 ? 1 : -1);
+	}
+
+	private void Update () {
+		patrolTimer += Time.deltaTime;
+		if (patrolTimer > patrolTime) {
+			rb.velocity = 0;
+			patrolTimer = 0;
+			direction *= -1;
+		}
+		rb.AddForce (direction * moveFroce * Time.deltaTime);
+	}
 }
